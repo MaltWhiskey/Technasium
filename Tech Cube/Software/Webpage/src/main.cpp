8b73c466c7c4d5506220917c1a76559290ee0321
btpv <villeriusborro@gmail.com>
2023-09-22 13:41:15 +0200

    add Webpage

--- Tech Cube/Software/Webpage/src/main.cpp
+++ Tech Cube/Software/Webpage/src/main.cpp
@@ -20,17 +20,73 @@ static const uint8_t height = 4;
 static const uint8_t depth = 4;
 static CRGB cube[width][height][depth];
 static CRGB leds[width * height * depth];
-String wsmsg = "hello";
+char *wsmsg;
 void notifyClients(String msg) {
   ws.textAll(msg);
 }
+void display_clear() { memset(cube, 0, sizeof(cube)); }
 
+// Move 3D Cube data to 1D LED array
+void display_update(int ms_delay) {
+  for (uint8_t x = 0; x < width; x++) {
+    uint8_t xx = x * height;
+    for (uint8_t y = 0; y < height; y++) {
+      uint8_t yy = (x & 1) ? height - 1 - y : y;
+      for (uint8_t z = 0; z < depth; z++) {
+        uint8_t zz = (z + (z & 1)) * height * width;
+        uint8_t led = (z & 1) ? zz - xx - yy - 1 : xx + yy + zz;
+        leds[led] = cube[x][y][z];
+      }
+    }
+  }
+  FastLED.show();
+  delay(ms_delay);
+}
+int hex_to_val(const char ch)
+{
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';  /* Simple ASCII arithmetic */
+    else if (ch >= 'a' && ch <= 'f')
+        return 10 + ch - 'a';  /* Because hex-digit a is ten */
+    else if (ch >= 'A' && ch <= 'F')
+        return 10 + ch - 'A';  /* Because hex-digit A is ten */
+    else
+        return 0;  /* Not a valid hexadecimal digit */
+}
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
       wsmsg = (char*)data;
       Serial.println(wsmsg);
       notifyClients(wsmsg);
+      byte x = 0;
+      byte y = 0;
+      byte z = 0;
+      for (int i = 0 ; i<len; i++){
+        uint32_t hex = 0;
+        if (data[i] == '#' && i+7 < len){
+          for(int j=0;j<6;j++) {
+            int chr = hex_to_val(data[j+i+1]);
+            hex<<=4;
+            hex |= chr;
+          }
+          i += 7;
+          cube[x][3-y][z] = hex;
+          x++;
+          if (x > 3){
+            y++;
+            x=0;
+            if (y > 3){
+              z++;
+              y=0;
+            }
+          }
+          Serial.println(hex);
+
+        }
+      }
+      display_update(0);
+      display_clear();
     //}
   }
 }
@@ -84,83 +140,37 @@ void setup() {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  Serial.println(bench("/index.html"));
-  Serial.println(bench("/style.css"));
-  Serial.println(bench("/favicon.ico"));
-  Serial.println(bench("/script/ws.js"));
-  Serial.println(bench("/script/sketch.js"));
-  Serial.println(bench("/script/setup.js"));
-  Serial.println(bench("/libraries/p5.min.js")); 
-
   Serial.println("Setting AP (Access Point)…");
   WiFi.softAP(ssid, password);
+  // Serial.println(bench("/index.html"));
+  // Serial.println(bench("/style.css"));
+  // Serial.println(bench("/favicon.ico"));
+  // Serial.println(bench("/script/ws.js"));  Serial.println(bench("/index.html"));
+  // Serial.println(bench("/style.css"));
+  // Serial.println(bench("/favicon.ico"));
+  // Serial.println(bench("/script/ws.js"));
+  // Serial.println(bench("/script/sketch.js"));
+  // Serial.println(bench("/script/setup.js"));
+  // Serial.println(bench("/libraries/p5.min.js")); 
+
+  // Serial.println(bench("/script/sketch.js"));
+  // Serial.println(bench("/script/setup.js"));
+  // Serial.println(bench("/libraries/p5.min.js")); 
+
 
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
   initWebSocket();
-  server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-  server.on("/script/ws.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/script/ws.js", "text/javascript");
-  });
-  server.on("/script/setup.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/script/setup.js", "text/javascript");
-  });
-  server.on("/script/sketch.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/script/sketch.js", "text/javascript");
-  });
-  server.on("/libraries/p5.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/libraries/p5.min.js", "text/javascript");
-  });
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/favicon.ico", "image/x-icon");
-  });
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
   server.begin();
 }
 
-// Move 3D Cube data to 1D LED array
-void display_update(int ms_delay) {
-  for (uint8_t x = 0; x < width; x++) {
-    uint8_t xx = x * height;
-    for (uint8_t y = 0; y < height; y++) {
-      uint8_t yy = (x & 1) ? height - 1 - y : y;
-      for (uint8_t z = 0; z < depth; z++) {
-        uint8_t zz = (z + (z & 1)) * height * width;
-        uint8_t led = (z & 1) ? zz - xx - yy - 1 : xx + yy + zz;
-        leds[led] = cube[x][y][z];
-      }
-    }
-  }
-  FastLED.show();
-  delay(ms_delay);
-}
 
-void display_clear() { memset(cube, 0, sizeof(cube)); }
 /*------------------------------------------------------------------------------
  * Start the main loop
  *----------------------------------------------------------------------------*/
 void loop() {
-  static int y = 0;
-  static int a = 1;
 
-  for (int x = 0; x < width; x++) {
-    cube[x][y][0] = CRGB(40, 0, 40);
-    cube[x][y][1] = CRGB(50, 0, 0);
-    cube[x][y][2] = CRGB(0, 80, 0);
-    cube[x][y][3] = CRGB(0, 0, 200);
-  }
-  int t = y + a;
-  if (t > height - 1 || t < 0) a = -a;
-  y += a;
-  display_update(200);
-  display_clear();
 }
