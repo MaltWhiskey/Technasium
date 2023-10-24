80aede297346cd879a0801fcddf23db4d4b4e599
MaltWhiskey <MaltWhiskey@outlook.com>
2023-10-24 07:38:11 +0200

    done

--- Halloween/Software/Pumpkin/src/main.cpp
+++ Halloween/Software/Pumpkin/src/main.cpp
@@ -1,25 +1,31 @@
 #include <Arduino.h>
 #include <stdint.h>
+
 #include "sample.h"
 /*------------------------------------------------------------------------------
  * Initialize setup parameters
  *----------------------------------------------------------------------------*/
+// RIGHT -> 39,37,35,33,18,16,GND,VBUS
+// LEFT  -> EN,3,5,7,9,11,12,3V3
+static const uint8_t LDR_PIN = 3;
 static const uint8_t DAC_PIN = 18;
-static const uint8_t LDR_PIN = 16;
-static const uint8_t OLED_PIN = 33;
-static const uint8_t WLED_PIN = 35;
+static const uint8_t PIR_PIN = 35;
+static const uint8_t OLED_PIN = 37;
+static const uint8_t WLED_PIN = 39;
 
-const sample_dsc_t sample[] = {  scream_dsc, hiss_dsc, thunder1_dsc};
-
-void play();
+const sample_dsc_t sample[] = {hiss_dsc, howl_dsc, magniet_dsc, scream_dsc,
+                               thunder_dsc};
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
   pinMode(DAC_PIN, OUTPUT);
   pinMode(LDR_PIN, INPUT);
+  pinMode(PIR_PIN, INPUT);
   pinMode(OLED_PIN, OUTPUT);
   pinMode(WLED_PIN, OUTPUT);
+  digitalWrite(OLED_PIN, LOW);
+  digitalWrite(OLED_PIN, HIGH);
 }
 /*------------------------------------------------------------------------------
  * Start the main loop
@@ -29,44 +35,42 @@ void loop() {
   static uint8_t nr = 0;
   static uint8_t wave;
   static unsigned long startTime = 0;
-  // Allow for 1 second LDR calibration
-  static unsigned long coolDown = millis() + 1000;
+  static unsigned long coolDown = 0;
+  static float fast_avg = 0.0;
+  static float slow_avg = 0.0;
+  const float fast_alpha = 0.050f;
+  const float slow_alpha = 0.005f;
+  const uint16_t threshold = 200;
 
-  static double ldr_avg = 0.0;
-  const float alpha = 0.3f;
-  const double deviation = 40;
-  
-  if(!play) {
+  if (!play) {
     uint16_t ldr_value = analogRead(LDR_PIN);
-    ldr_avg = (ldr_value * alpha) + (ldr_avg * (1 - alpha));
-    if(millis() >= coolDown) {
-      if(abs(ldr_avg - ldr_value) > deviation) {
-        play = true;
-        startTime = micros();
-      }
-    } 
-  }
-  else {
+    uint16_t pir_value = digitalRead(PIR_PIN);
+    fast_avg = (ldr_value * fast_alpha) + (fast_avg * (1 - fast_alpha));
+    slow_avg = (ldr_value * slow_alpha) + (slow_avg * (1 - slow_alpha));
+    uint16_t deviation = abs(slow_avg - fast_avg);
+
+    if (millis() >= coolDown && (deviation > threshold)) {
+      play = true;
+      startTime = micros();
+    }
+
+  } else {
+    digitalWrite(OLED_PIN, LOW);
     unsigned long pos = (micros() - startTime) / (1000000 / sample[nr].rate);
-    if(pos < sample[nr].size) {
+    if (pos < sample[nr].size) {
       wave = sample[nr].data[pos];
       dacWrite(DAC_PIN, wave);
-      if (wave < 80 || wave > 255-80) {
-        digitalWrite(OLED_PIN, LOW);
+      if (wave < 90 || wave > 255 - 90) {
         digitalWrite(WLED_PIN, HIGH);
-      }
-      else {
+      } else {
         digitalWrite(WLED_PIN, LOW);
-        digitalWrite(OLED_PIN, HIGH);
       }
-    }
-    else
-    {
-      nr=(++nr)%3;
+    } else {
       play = false;
-      coolDown = millis() + 1000;
+      nr = (++nr) % 5;  // sizeof(sample);
       digitalWrite(WLED_PIN, LOW);
       digitalWrite(OLED_PIN, HIGH);
-    } 
+      coolDown = millis() + 1000;
+    }
   }
 }
--- Halloween/Software/Pumpkin/src/sample.h
+++ Halloween/Software/Pumpkin/src/sample.h
@@ -1,13 +1,20 @@
 /*
-python -c "import sys;a=sys.argv;open(a[2],'wb').write(('const unsigned char '+a[3]+'[] = {'+','.join([hex(b) for b in open(a[1],'rb').read()])+'};').encode('utf-8'))" <binary file> <header file> <array name>
+python -c "import sys;a=sys.argv;open(a[2],'wb').write(('const unsigned char
+'+a[3]+'[] = {'+','.join([hex(b) for b in
+open(a[1],'rb').read()])+'};').encode('utf-8'))" <binary file> <header file>
+<array name>
+
+xxd -i file.raw > file.h
+
 */
 
 #include <stdint.h>
+
 #include "hiss.h"
-//#include "howl.h"
+#include "howl.h"
+#include "magniet.h"
 #include "scream.h"
-#include "thunder1.h"
-//#include "magniet.h"
+#include "thunder.h"
 
 struct sample_dsc_t {
   const uint32_t size;
@@ -15,8 +22,8 @@ struct sample_dsc_t {
   const uint8_t *data;
 };
 
-//const sample_dsc_t howl_dsc = {sizeof(howl), 44100, howl};
-const sample_dsc_t scream_dsc = {sizeof(scream), 44100, scream};
-const sample_dsc_t hiss_dsc = {sizeof(hiss), 44100, hiss};
-const sample_dsc_t thunder1_dsc = {sizeof(thunder1), 44100, thunder1};
-//const sample_dsc_t magniet_dsc = {sizeof(magniet), 44100, magniet};                               
+const sample_dsc_t howl_dsc = {sizeof(howl), 11025, howl};
+const sample_dsc_t scream_dsc = {sizeof(scream), 11025, scream};
+const sample_dsc_t hiss_dsc = {sizeof(hiss), 11025, hiss};
+const sample_dsc_t thunder_dsc = {sizeof(thunder), 11025, thunder};
+const sample_dsc_t magniet_dsc = {sizeof(magniet), 11025, magniet};
