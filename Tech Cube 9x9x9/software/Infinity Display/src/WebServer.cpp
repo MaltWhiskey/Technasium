f271cdf8baddf3a987b2fff3e1c7f26561335f0e
MaltWhiskey <MaltWhiskey@outlook.com>
2023-11-06 05:47:55 +0100

    added webserver

---
+++ Tech Cube 9x9x9/software/Infinity Display/src/WebServer.cpp
@@ -0,0 +1,158 @@
+#include "WebServer.h"
+
+#include <Arduino.h>
+#include <DNSServer.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
+#include <SPIFFS.h>
+#include <WebSocketsServer.h>
+#include <WiFi.h>
+
+#include "ArduinoJson.h"
+#include "Core/Config.h"
+
+#define WEBSERVER_PORT 80
+#define WEBSOCKET_PORT 1337
+#define ACCESSPOINT_IP 192, 168, 1, 1
+#define ACCESSPOINT_SSID "CUBE"
+#define ACCESSPOINT_PASS "^3"
+#define DNSSERVER_PORT 53
+// This json document size has plenty of space to hold commands send to the gui.
+#define COMMAND_DOC_SIZE 255
+
+namespace WebServer {
+
+AsyncWebServer server = AsyncWebServer(WEBSERVER_PORT);
+WebSocketsServer webSocket = WebSocketsServer(WEBSOCKET_PORT);
+IPAddress APIP = IPAddress(ACCESSPOINT_IP);
+DNSServer dnsServer;
+boolean AP_MODE;
+
+void begin() {
+  // Start file system
+  if (!SPIFFS.begin()) {
+    Serial.println("Error mounting SPIFFS");
+  }
+  // Start WiFi
+  Serial.print("Starting WiFi");
+  WiFi.begin(config.network.wifi.ssid, config.network.wifi.password);
+  AP_MODE = true;
+  for (uint8_t i = 0; i < 70; i++) {
+    if (WiFi.status() == WL_CONNECTED) {
+      AP_MODE = false;
+      break;
+    }
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  // Start as Accesspoint
+  if (AP_MODE) {
+    Serial.println("Starting Wifi Access Point");
+    WiFi.mode(WIFI_AP);
+    // Quick hack to wait for SYSTEM_EVENT_AP_START
+    delay(100);
+    WiFi.softAPConfig(APIP, APIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(ACCESSPOINT_SSID, ACCESSPOINT_PASS);
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
+    dnsServer.start(DNSSERVER_PORT, "*", APIP);
+  }
+
+  // Start connected to local network
+  if (!AP_MODE) {
+    Serial.print("Connected to ");
+    Serial.println(config.network.wifi.ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    // Start MDNS
+    if (MDNS.begin(config.network.server.hostname), WiFi.localIP()) {
+      Serial.print("MDNS responder started. Hostname = ");
+      Serial.println(config.network.server.hostname);
+    }
+    // Add service to MDNS-SD
+    MDNS.addService("http", "tcp", 80);
+  }
+
+  // On HTTP request for root, provide index.html file
+  server.on("*", HTTP_GET, onIndexRequest);
+  // Start web server
+  server.begin();
+  // Start WebSocket server and assign callback
+  webSocket.onEvent(onWebSocketEvent);
+  // Start web socket
+  webSocket.begin();
+}
+
+// Handle network traffic
+void update() {
+  // Handle WebSocket data
+  webSocket.loop();
+  if (AP_MODE) {
+    // Handle dns requests in Access Point Mode only
+    dnsServer.processNextRequest();
+  } else if (WiFi.status() != WL_CONNECTED) {
+    // Handle reconnects only when connected to Lan
+    begin();
+  }
+}
+
+void onIndexRequest(AsyncWebServerRequest* request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() + "] HTTP GET request of " +
+                 request->url());
+  if (request->url().equals("/"))
+    request->send(SPIFFS, "/index.html", "text/html");
+  else if (request->url().equals("/gui.json")) {
+    String buffer;
+    // config.serialize(buffer);
+    request->send(200, "application/json", buffer);
+  } else if (SPIFFS.exists(request->url()))
+    if (request->url().endsWith(".html")) {
+      request->send(SPIFFS, request->url(), "text/html");
+    } else if (request->url().endsWith(".css")) {
+      request->send(SPIFFS, request->url(), "text/css");
+    } else if (request->url().endsWith(".json")) {
+      request->send(SPIFFS, request->url(), "application/json");
+    } else if (request->url().endsWith(".ico")) {
+      request->send(SPIFFS, request->url(), "image/x-icon");
+    } else if (request->url().endsWith(".png")) {
+      request->send(SPIFFS, request->url(), "image/png");
+    } else {
+      request->send(404, "text/plain", "Mime-Type Not Found");
+    }
+  else {
+    // request->send(404, "text/plain", "Not Found");
+    request->redirect("/index.html");
+  }
+}
+
+void onWebSocketEvent(uint8_t client_num, WStype_t type, uint8_t* payload,
+                      size_t length) {
+  switch (type) {
+    // Client has disconnected
+    case WStype_DISCONNECTED: {
+      Serial.printf("Disconnected client %u\n", client_num);
+    } break;
+    // New client has connected
+    case WStype_CONNECTED: {
+      Serial.printf("Connection from client %u\n", client_num);
+    } break;
+    // Text received from a connected client
+    case WStype_TEXT: {
+      // config.execute(payload);
+    } break;
+    case WStype_BIN:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    case WStype_ERROR:
+    case WStype_PING:
+    case WStype_PONG:
+      break;
+  }
+}
+
+}  // namespace WebServer
--- Tech Cube 9x9x9/software/Infinity Display/src/main.cpp
+++ Tech Cube 9x9x9/software/Infinity Display/src/main.cpp
@@ -3,29 +3,68 @@
 
 #include "Core/Config.h"
 #include "Space/animation.h"
-/*---------------------------------------------------------------------------------------
+#include "WebServer.h"
+/*------------------------------------------------------------------------------
  * Globals
- *-------------------------------------------------------------------------------------*/
+ *----------------------------------------------------------------------------*/
+// Global configuration parameters
 Config config;
+// TaskHandles for running tasks on different cores
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+// Forward declare task functions
+void task1(void *);
+void task2(void *);
 /*------------------------------------------------------------------------------
  * Initialize setup parameters
  *----------------------------------------------------------------------------*/
 void setup() {
-  Animation::begin();
+  // Enable console logging
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("Starting");
+  // Initialize animation display
+  Animation::begin();
+  // Load config from file system
+  // config.load();
+  // Initialize web server communication
+  WebServer::begin();
+  // Create task1 on core 0 (higher number is higher priority)
+  xTaskCreatePinnedToCore(task1, "Task1", 10000, NULL, 10, &Task1, 0);
+  delay(500);
+  // Create task2 on core 1 (higher number is higher priority)
+  xTaskCreatePinnedToCore(task2, "Task2", 10000, NULL, 20, &Task2, 1);
+  delay(500);
 }
 /*------------------------------------------------------------------------------
- * Start the main loop
+ * Loop Core 1
  *----------------------------------------------------------------------------*/
-void loop() {
-  // Print FPS once every second
-  static Timer print_interval = 1.0f;
-  Animation::loop();
-  if (print_interval.update()) {
-    Serial.print("fps=");
-    Serial.print(Animation::fps());
-    Serial.println();
+void loop() { delay(1000); }
+/*------------------------------------------------------------------------------
+ * Task1 Core 0
+ *----------------------------------------------------------------------------*/
+void task1(void *parameter) {
+  Serial.printf("Task1: Wifi running on core %d\n", xPortGetCoreID());
+  while (true) {
+    // Check for Web server events
+    WebServer::update();
+    // Prevents watchdog timeout
+    vTaskDelay(1);
+  }
+}
+/*------------------------------------------------------------------------------
+ * Task2 Core 1
+ *----------------------------------------------------------------------------*/
+void task2(void *parameter) {
+  Serial.printf("Task2: Animations running on core %d\n", xPortGetCoreID());
+  // Run animation on blocking fastled. Interrupts mess up the led data timeing
+  // Need a way to disable interrupts and watchdog on Core 1 or use a
+  // nonblocking FastLed controller (DMA based?)
+  while (true) {
+    // Print FPS once every second
+    static Timer print_interval = 1.0f;
+    // Run animation rountines and update the display
+    Animation::loop();
+    if (print_interval.update()) {
+      Serial.printf("fps=%f\n", Animation::fps());
+    }
   }
 }
