fe4872f830470c4ca3aad1c49297fbaa34d5f2c0
MaltWhiskey <MaltWhiskey@outlook.com>
2024-06-26 14:56:29 +0200

    trrtaert

--- Tech Cube/Software/Chalieplex/src/main.cpp
+++ Tech Cube/Software/Chalieplex/src/main.cpp
@@ -40,12 +40,13 @@ void blink() {
   static unsigned long ms = millis();
   static float phase = 0;
   static uint8_t line = 0;
+  static uint8_t colors[3];
 
+  // COLOR CALCULATIONS  
   phase += 0.001f * (millis() - ms); ms = millis();
   float s1 = sinf(phase * 1.0f);
   float s2 = sinf(phase * 1.1f);
   float s3 = sinf(phase * 1.2f);
-
   etage[0].green = s1 * s1 * 0xff;
   etage[1].blue = s2 * s2 * 0xff;
   etage[2].red = s3 * s3 * 0xff;
@@ -53,12 +54,11 @@ void blink() {
   etage[3].blue = etage[1].blue;
   etage[3].red = etage[2].red;
 
-  // SCAN LINE SELECTIE, OUTPUT HIGH
+  // CONNECT POWER
   pinMode(pin[line], OUTPUT);
   digitalWrite(pin[line], 1);
 
-  uint8_t colors[3];
-
+  // COLORS TO GND, OTHERS OPEN
   for(uint8_t x = 0; x < 3 ; x++) {
     colors[x] = etage[line].colors[x];
     uint8_t index = (line + 1 + x)&3;
@@ -69,16 +69,20 @@ void blink() {
     else 
       pinMode(pin[index], INPUT);
   }
+
+  // DELAY SWITCHING
+  delayMicroseconds(pow(1.0015f, analogRead(pot)) - 1);
+
+  // PERFORM PWM
   for(uint16_t t = 0; t <= 0xff; t++) {
-    delayMicroseconds(15);
+    delayMicroseconds(5);
     for(uint8_t x = 0; x < 3 ; x++) {
       uint8_t index = (line + 1 + x)&3;
       if(colors[x]-- == 0)
         pinMode(pin[index], INPUT);
     }
   }
-  delayMicroseconds(pow(1.0015f, analogRead(pot)) - 1);
-  // DISCONNECT LED AT SCAN LINE FIRST
+  // DISCONNECT POWER
   pinMode(pin[line], INPUT);
   line = (line+1)&3;
 }
